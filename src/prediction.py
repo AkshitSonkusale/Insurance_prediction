ae1e72308daa15b2dec8495a26bf3a6d3b1c887a
Akshit Sonkusale <159681608+AkshitSonkusale@users.noreply.github.com>
2026-03-11 14:00:17 +0530

    Update prediction.py

--- src/prediction.py
+++ src/prediction.py
@@ -1,26 +1,22 @@
 import pickle
 import numpy as np
 
-class Insurance_prediction:
+class Insurance_Prediction:
 
     def __init__(self):
-        with open("artifacts/scaler.pkl","rb") as f:
+
+        with open(r"artifacts/scaler.pkl","rb") as f:
             self.scaler = pickle.load(f)
 
-        with open("artifacts/model.pkl","rb") as f:
+        with open(r"artifacts/model.pkl","rb") as f:
             self.model = pickle.load(f)
 
     def prediction(self, Age, Annual_Income_LPA, Policy_Term_Years, Sum_Assured_Lakhs):
 
-        input = np.array([[Age, Annual_Income_LPA, Policy_Term_Years, Sum_Assured_Lakhs]])
+        input_data = np.array([[Age, Annual_Income_LPA, Policy_Term_Years, Sum_Assured_Lakhs]])
 
-        scaled_input = self.scaler.transform(input)
+        scaled_input = self.scaler.transform(input_data)
 
         result = self.model.predict(scaled_input)
 
         return result[0]
-<<<<<<< HEAD
-=======
-    
-        print(self.scaler.n_features_in_)
->>>>>>> 23c26cd (updated model training)
