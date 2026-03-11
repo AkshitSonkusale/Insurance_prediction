0b2c8a03c4417208160c1abe680ef2d9a9f9f273
Akshit Sonkusale <159681608+AkshitSonkusale@users.noreply.github.com>
2026-03-11 12:20:50 +0530

    Add Annual_Premium_Thousands parameter to prediction

--- src/prediction.py
+++ src/prediction.py
@@ -17,12 +17,12 @@ class Insurance_prediction:
         with open(model_path, "rb") as f:
             self.model = pickle.load(f)
 
-    def prediction(self, Age, Annual_Income_LPA, Policy_Term_Years, Sum_Assured_Lakhs):
+   def prediction(self, Age, Annual_Income_LPA, Policy_Term_Years, Sum_Assured_Lakhs, Annual_Premium_Thousands):
 
-        input_data = np.array([[Age, Annual_Income_LPA, Policy_Term_Years, Sum_Assured_Lakhs]])
+    input_data = np.array([[Age, Annual_Income_LPA, Policy_Term_Years, Sum_Assured_Lakhs, Annual_Premium_Thousands]])
 
-        scaled_input = self.scaler.transform(input_data)
+    scaled_input = self.scaler.transform(input_data)
 
-        result = self.model.predict(scaled_input)
+    result = self.model.predict(scaled_input)
 
-        return result[0]
+    return result[0]
