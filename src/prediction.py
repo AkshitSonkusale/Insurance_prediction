31784ac68992cc100f8079930b42bcbad16c4bbe
Akshit Sonkusale <159681608+AkshitSonkusale@users.noreply.github.com>
2026-03-11 12:29:10 +0530

    Refactor prediction method and file paths

--- src/prediction.py
+++ src/prediction.py
@@ -1,28 +1,21 @@
 import pickle
 import numpy as np
-import os
 
 class Insurance_prediction:
 
     def __init__(self):
-
-        base_dir = os.path.dirname(os.path.dirname(__file__))
-
-        scaler_path = os.path.join(base_dir, "artifacts", "scaler.pkl")
-        model_path = os.path.join(base_dir, "artifacts", "model.pkl")
-
-        with open(scaler_path, "rb") as f:
+        with open("artifacts/scaler.pkl","rb") as f:
             self.scaler = pickle.load(f)
 
-        with open(model_path, "rb") as f:
+        with open("artifacts/model.pkl","rb") as f:
             self.model = pickle.load(f)
 
-   def prediction(self, Age, Annual_Income_LPA, Policy_Term_Years, Sum_Assured_Lakhs, Annual_Premium_Thousands):
+    def prediction(self, Age, Annual_Income_LPA, Policy_Term_Years, Sum_Assured_Lakhs):
 
-    input_data = np.array([[Age, Annual_Income_LPA, Policy_Term_Years, Sum_Assured_Lakhs, Annual_Premium_Thousands]])
+        input = np.array([[Age, Annual_Income_LPA, Policy_Term_Years, Sum_Assured_Lakhs]])
 
-    scaled_input = self.scaler.transform(input_data)
+        scaled_input = self.scaler.transform(input)
 
-    result = self.model.predict(scaled_input)
+        result = self.model.predict(scaled_input)
 
-    return result[0]
+        return result[0]
